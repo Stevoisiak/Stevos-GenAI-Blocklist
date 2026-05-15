f30cc903825e50f0e48c73d27d1b3d717928b6b6
Stevoisiak <stevoisiak@gmail.com>
2026-05-15 12:19:54 -0400

    Print filter descriptions

--- scripts/blocklist-maker.py
+++ scripts/blocklist-maker.py
@@ -5,39 +5,41 @@ with open('reddit.txt', 'r') as file:
 
 
 # -- https://www.reddit.com/
-# Posts from AI Subreddit
+print('! Posts from AI subreddits')
 test = 'www.reddit.com##shreddit-post:is([subreddit-name="' + '"],[subreddit-name="'.join(reddit_list) + '"])'
 print(test + '\n')
 
-# Right sidebar - Recent posts from AI subreddits
+print('! Right sidebar - Recent posts from AI subreddits')
 test = 'www.reddit.com##[slot="posts"] > div:has(faceplate-hovercard:is([label="r/' + '"],[label="r/'.join(reddit_list) + '"]))'
 print(test + '\n')
 
 # https://www.reddit.com/search/?q=ai (Search results)
-# Media & Posts from AI subreddits
+print('! Media & Posts from AI subreddits')
 test = 'www.reddit.com##search-telemetry-tracker[view-events="search/view/post"]:has([aria-haspopup="dialog"]:is([href="/r/' + '/"],[href="/r/'.join(reddit_list) + '/"]))'
 print(test + '\n')
 
 # Search communities
+print('! Communities (AI subreddits)')
 test = 'www.reddit.com##[data-testid="search-community"]:has(a:is([href="/r/' + '/"],[href="/r/'.join(reddit_list) + '/"]))'
 print(test + '\n')
 
 # ============================
 
 # -- https://old.reddit.com/
-# Top navbar > My Subreddits - AI Subreddits
+print('! Top navbar > My Subreddits - AI Subreddits')
 test = 'old.reddit.com##.sr-bar > li:has(> :is([href$="/r/' + '/"],[href$="/r/'.join(reddit_list) + '/"]))'
 print(test + '\n')
 
-# Post from AI subreddit
+print('! Post from AI subreddit')
 test = 'old.reddit.com##.link:is([data-subreddit="' + '"],[data-subreddit="'.join(reddit_list) + '"])'
 print(test + '\n')
 
 # -- https://old.reddit.com/search?q=ai+art
-# Search result from AI Subreddit
+print('! Search results for subreddits and posts from AI subreddits')
 test = 'old.reddit.com##.search-result:has( .search-subreddit-link:is([href$="/r/' + '/"],[href$="/r/'.join(reddit_list) + '/"]))'
 print(test + '\n')
 
 # -- https://old.reddit.com/subreddits/search?q=ai
+print('! Search results for AI subreddits')
 test = 'old.reddit.com##.subreddit:has([data-sr_name="' + '"],[data-sr_name="'.join(reddit_list) + '"])'
 print(test)
